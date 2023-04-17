ec55c1d9c8b92dd67ee6940f3d258aa2c06e0b0f
Marc Evanstein <marc@marcevanstein.com>
2023-04-17 10:00:55 -0700

    4/17/2023 -- made element properties accessible by camelcase

--- marciano/periodictable.py
+++ marciano/periodictable.py
@@ -5,6 +5,11 @@ import pathlib
 
 pt_dataframe = pd.read_csv(pathlib.Path(__file__).parent / "PeriodicTable.csv")
 pt_dataframe.set_index("Element", drop=False, inplace=True)
+pt_dataframe["Electronegativity"].fillna(0, inplace=True)
+
+
+def _snake_to_camel(s):
+    return ''.join(x.title() for x in s.split("_"))
 
 
 class Element:
@@ -23,15 +28,23 @@ class Element:
     def phase_at(self, kelvin_temp):
         if self.AtomicNumber > 99:
             raise ValueError("Phase undefined for elements number 100 and higher.")
-        elif kelvin_temp >= self.BoilingPoint:
+        elif kelvin_temp >= self.boiling_point:
             return "gas"
-        elif kelvin_temp >= self.MeltingPoint or np.isnan(self.MeltingPoint):
+        elif kelvin_temp >= self.melting_point or np.isnan(self.melting_point):
             return "liquid"
         else:
             return "solid"
         
+    def discovered_by(self, year):
+        return True if np.isnan(self.year) else year >= self.year
+        
     def __getattr__(self, attr):
-        return self.element_info[attr]
+        if attr in self.element_info:        
+            return self.element_info[attr]
+        elif (camel_version := _snake_to_camel(attr)) in self.element_info:
+            return self.element_info[camel_version]
+        else:
+            raise AttributeError(f"Element does not have attribute '{attr}'")
     
     def __repr__(self):
         return f"Element({self.AtomicNumber})"
